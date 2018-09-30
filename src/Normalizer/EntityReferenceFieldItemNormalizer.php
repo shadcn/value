030ae4e9cc69c03f30dd27833f7386824bc94863
Arshad Chummun <arshad@chummun.com>
2018-09-30 23:28:58 +0400

    Issue #2996556: Infinite recursion when two entities reference each other in an entity reference field

--- src/Normalizer/EntityReferenceFieldItemNormalizer.php
+++ src/Normalizer/EntityReferenceFieldItemNormalizer.php
@@ -3,6 +3,7 @@
 namespace Drupal\value\Normalizer;
 
 use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\field\Entity\FieldConfig;
 
 class EntityReferenceFieldItemNormalizer extends NormalizerBase {
 
@@ -15,8 +16,27 @@ class EntityReferenceFieldItemNormalizer extends NormalizerBase {
    * {@inheritdoc}
    */
   public function normalize($object, $format = NULL, array $context = []) {
-    $entity = $object->get('entity')->getValue();
-    $attributes = $this->serializer->normalize($entity, $format, $context);
+    $attributes = [];
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
+    if ($entity = $object->get('entity')->getValue()) {
+      $attributes = [
+        'target_type' => $entity->getEntityTypeId(),
+        'target_uuid' => $entity->uuid(),
+        'target_id' => $entity->id(),
+      ];
+
+      $definition = $object->getFieldDefinition();
+      if ($definition instanceof FieldConfig) {
+        $id = $definition->id();
+        $key = "{$id}-{$entity->id()}";
+
+        if (!isset($context['depths'][$key])) {
+          $context['depths'][$key] = [];
+          $attributes = $this->serializer->normalize($entity, $format, $context);
+        }
+      }
+    }
+
     return $attributes;
   }
 }
